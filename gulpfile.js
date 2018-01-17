8a48c494028c9ff8183b92ad532cf19e18f38815
Chris Chapman <Chapman@apextion.com>
2018-01-17 12:22:14 -0500

    working static pkg json saving

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,23 @@
 const gulp = require('gulp');
+const fs = require('fs');
+const argv = require('yargs').argv
 
 
+gulp.task('version', () => {
+  // configs
+  const newVersion = 'v1.1.0';
+  const targetFilePath = './package.json';
 
-gulp.task('tag', () => {
-  console.log('Base hit');
+  // Increment pkg json
+  const pkg = require(targetFilePath);
+  pkg.version = newVersion
+
+  fs.writeFile(targetFilePath, JSON.stringify(pkg), (err) => {
+    if (err) throw err;
+    console.log(targetFilePath + ' has been updated to ' + newVersion);
+  });
+
+  console.log(argv);
+
+  // add tag to commit
 })
