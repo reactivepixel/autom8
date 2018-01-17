90a42fcb12271cdd3063b5c0729e066ee7046096
Chris Chapman <Chapman@apextion.com>
2018-01-17 12:58:33 -0500

    finished demo for class

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,51 @@
 const gulp = require('gulp');
 const fs = require('fs');
 const argv = require('yargs').argv
-
+const git = require('gulp-git');
 
 gulp.task('version', () => {
   // configs
-  const newVersion = 'v1.1.0';
   const targetFilePath = './package.json';
 
   // Increment pkg json
   const pkg = require(targetFilePath);
-  pkg.version = newVersion
+  pkg.version = versionBump(pkg.version);
 
   fs.writeFile(targetFilePath, JSON.stringify(pkg), (err) => {
     if (err) throw err;
-    console.log(targetFilePath + ' has been updated to ' + newVersion);
+    console.log(targetFilePath + ' has been updated to ' + pkg.version);
   });
 
-  console.log(argv);
+ //  git.tag(pkg.version, 'Automated Bumping', (err) => {
+ //   if (err) throw err;
+ // });
 
-  // add tag to commit
 })
+
+
+const versionBump = (initialVersion, type) => {
+  const formattedVersion = initialVersion.substr(1, initialVersion.length)
+  const parsedVersion = formattedVersion.split('.'); // [v1,2,0]
+
+  for(position in parsedVersion) {
+    const versionNumber = parsedVersion[position];
+    parsedVersion[position] = parseInt(versionNumber);
+  }
+
+
+  if(argv.type === 'major') {
+    const parsedInt = parseInt(parsedVersion[0])
+    parsedVersion[0] = parsedInt + 1;
+    parsedVersion[1] = 0;
+    parsedVersion[2] = 0;
+  } else if(argv.type === 'minor') {
+    const parsedInt = parseInt(parsedVersion[1])
+    parsedVersion[1] = parsedInt + 1;
+    parsedVersion[2] = 0;
+  } else if(argv.type === 'patch') {
+    const parsedInt = parseInt(parsedVersion[2])
+    parsedVersion[2] = parsedInt + 1;
+  }
+
+  return 'v' + parsedVersion.join('.');
+}
